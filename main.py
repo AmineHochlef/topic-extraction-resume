21bd5138649e5808c9123bf7717f9794e0a464f1
aminehochlef <hochamine22@gmail.com>
2024-03-04 18:17:24 +0100

    extracting images from a pdf file

--- main.py
+++ main.py
@@ -2,6 +2,12 @@ import re
 from pdfminer.high_level import extract_pages, extract_text
 # tabula library for extracting data from tables in a pdf 
 import tabula
+# fitz from PyMuPDF - PIL.Image from Pillow
+# libraries for extracting images data
+import fitz
+import PIL.Image
+import io # for working with image data 
+
 
 # extracting simple text from the pdf
 text = extract_text("aminehochlef-sample-pdf-file.pdf")
@@ -19,3 +25,18 @@ df = tables[0]
 print(df.head(2))
 
 
+# extracting images from pdf file
+pdf = fitz.open("aminehochlef-sample-pdf-file.pdf")
+counter = 1
+for i in range(len(pdf)): # len(pdf) -> number of pages
+    page = pdf[i]
+    images = page.get_images()
+    for image in images:
+        base_img = pdf.extract_image(image[0])
+        image_data = base_img["image"]
+        img = PIL.Image.open(io.BytesIO(image_data))
+        extention = base_img["ext"]
+        img.save(open(f"image{counter}.{extention}", "wb"))
+        counter += 1
+# The first image from the pdf gets saved in the same directory 
+# as the py file with the name image<number of image in the pdf>.jpeg
